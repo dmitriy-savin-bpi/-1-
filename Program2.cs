6fd818c450dc12001c1d9edbbd9cbb7b179965db
dmitriy-savin-bpi <arturlejvin74@gmail.com>
2026-04-27 00:48:31 +0500

    Update Program2.cs

--- Program2.cs
+++ Program2.cs
@@ -1,33 +1,31 @@
-﻿using System;
+using System;
 
 namespace Variant20_Lab2
 {
     class Program
     {
-        // Рекурсивная функция для нахождения максимума
         static int FindMax(int[] arr, int n)
         {
-            if (n == 1) //если остался 1 элемент он и будет максимальный
-                return arr[0]; //первый элемент массива
+            if (n == 1)
+                return arr[0];
 
-            int maxPrev = FindMax(arr, n - 1); //вызываем ту же ф-ю, но уменьш. на 1 кол-во элементов (рекурсивный вызов)
-            return arr[n - 1] > maxPrev ? arr[n - 1] : maxPrev; //срав с макс пред части, возвр пред знач
+            int maxPrev = FindMax(arr, n - 1);
+            return arr[n - 1] > maxPrev ? arr[n - 1] : maxPrev;
         }
 
-        // Универсальная функция для безопасного ввода целого числа
         static void SafeInput(string prompt, out int value, bool positiveOnly = false)
         {
             value = 0;
-            bool valid = false; //флаг коррект ввода
+            bool valid = false;
 
-            while (!valid) //пока ввод не станет корр 
+            while (!valid)
             {
                 Console.Write(prompt);
                 string input = Console.ReadLine();
 
-                if (int.TryParse(input, out value)) //проверка явл целым числом
+                if (int.TryParse(input, out value))
                 {
-                    if (!positiveOnly || value > 0) //если не треб полож чило, усл сразу истинно
+                    if (!positiveOnly || value > 0)
                         valid = true;
                     else
                         Console.WriteLine("Ошибка: число должно быть положительным.");
@@ -43,20 +41,17 @@ namespace Variant20_Lab2
         {
             Console.WriteLine("=== Поиск максимального элемента в массиве (рекурсия) ===");
 
-            // Ввод размера массива
             SafeInput("Введите размер массива: ", out int n, true);
 
-            int[] array = new int[n]; //создание массива размером n
+            int[] array = new int[n];
 
-            // Ввод элементов массива
             Console.WriteLine("Введите элементы массива:");
-            for (int i = 0; i < n; i++) //от 0 до n-1
+            for (int i = 0; i < n; i++)
             {
-                SafeInput($"Элемент [{i + 1}]: ", out array[i]); //интерполяция строк и сохр введ число в i-й эл массива
+                SafeInput($"Элемент [{i + 1}]: ", out array[i]);
             }
 
-            // Вызов рекурсивной функции и вывод результата
-            int maxElement = FindMax(array, n); //вызов рекурс ф-ии
+            int maxElement = FindMax(array, n);
             Console.WriteLine($"\nМаксимальный элемент в массиве: {maxElement}");
 
             Console.WriteLine("\nНажмите любую клавишу для выхода...");
